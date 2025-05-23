c53ff287ddf72a1262dd411a126257459f584591
deyvidblendal <deyvid.blendal98@gmail.com>
2025-05-23 08:39:48 -0300

    (22/05) Habilitados navegação e modo escuro

---
+++ karangos/src/App.jsx
@@ -0,0 +1,43 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// import './App.css'
+
+import CssBaseline from '@mui/material/CssBaseline'
+
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './ui/theme'
+
+import TopBar from './ui/TopBar'
+import FooterBar from './ui/FooterBar'
+
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
+import Box from '@mui/material/Box'
+
+function App() {
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        
+        <BrowserRouter>
+          <TopBar />
+
+          {/* Dentro da prop "sx", "m" significa "margin" */}
+          <Box id="innerRoot" sx={{ m: '48px 24px' }}>
+            <AppRoutes />
+          </Box>
+
+          <FooterBar />
+        </BrowserRouter>
+
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
